8927321138a780d04d2744418da9819a66a29ce0
Tal Schwartz <email": null>
2016-01-12 13:48:12 -0800

    port google and github auth code, begin to update callback routes

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,4 +1,4 @@
-get '/sessions/new' do
+get '/login' do
   if session[:user_id]
     @user = User.find(session[:user_id])
     redirect "/users/#{@user.id}"
@@ -6,6 +6,14 @@ get '/sessions/new' do
     erb :login
   end
 end
+
+get 'login/github' do
+  session[:referrer] = request.referrer
+  client = Octokit::Client.new
+  github_login_url = client.authorize_url(CLIENT_ID, :scope => 'user:email')
+  redirect github_login_url
+end
+
 
 post '/sessions/new' do
   @user = User.find_by_email(params[:email])
@@ -22,5 +30,31 @@ end
 
 get '/logout' do
   session.delete(:user_id)
+  # delete all the session related stuff
+  session.delete()
   redirect '/'
+end
+
+
+get '/auth/github/callback' do
+  session_code = request.env['rack.request.query_hash']['code']
+  result = Octokit.exchange_code_for_token(session_code, CLIENT_ID, CLIENT_SECRET)
+  access_token = result[:access_token]
+
+  github_user_data = get_github_user_data(access_token)
+
+  # if there's a user in the db with the currently logged in github username, return that user
+  # otherwise show the registration form with the github info prefilled in the registration form
+  user =  User.find_by_github_username(github_user_data.login)
+  if user.nil?
+    @github_username = github_user_data.login
+    @github_fullname = github_user_data.name
+    @github_email = github_user_data.private_emails.first
+    @github_avatar_url = github_user_data.avatar_url
+    erb :"user/new"
+  else
+    session[:access_token] = access_token
+    session[:username] = github_user_data.login
+    redirect session[:referrer]
+  end
 end
---
+++ app/helpers/github_auth.rb
@@ -0,0 +1,28 @@
+def get_github_user_data(access_token)
+
+# make sure the token is still valid
+client = Octokit::Client.new \
+:client_id => CLIENT_ID,
+:client_secret => CLIENT_SECRET
+
+begin
+  client.check_application_authorization access_token
+rescue => e
+  # request didn't succeed because the token was revoked so we
+  # invalidate the token stored in the session and render the
+  # index page so that the user can start the OAuth flow again
+  session[:access_token] = nil
+  redirect '/login'
+end
+
+# if valid, return the user's data
+client = Octokit::Client.new :access_token => access_token
+user_data = client.user
+
+# find all emails and add it to user_data
+if client.scopes(access_token).include? 'user:email'
+  user_data['private_emails'] = client.emails.map { |m| m[:email] }
+end
+
+return user_data
+end
