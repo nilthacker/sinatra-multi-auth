d01f15a8bcfabad18383f48f368560f005eed226
Tal Schwartz <email": null>
2016-01-15 15:13:05 -0800

    set default avatar url to one generated from adorable avatar api

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   has_many :auths
 
+  after_initialize :set_default_values
+
+  def set_default_values
+    self.avatar_url ||= 'http://api.adorable.io/avatars/225/' + self.email
+  end
+
   def password
     @password ||= BCrypt::Password.new(password_hash)
   end
